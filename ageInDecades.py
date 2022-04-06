d349eea99c400d1f78d91062d35aa14c4d1a9f25
siobhan-doherty <siobhan-doherty@users.noreply.github.com>
2022-04-06 17:41:15 +0200

    Create ageInDecades.py

---
+++ ageInDecades.py
@@ -0,0 +1,6 @@
+# how many decades old are you
+
+age = int(input("How old are you?\n"))
+decades = age // 10
+years = age % 10
+print("You are " + str(decades) + " decades and " + str(years) + " years old.")
